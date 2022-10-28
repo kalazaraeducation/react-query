ccd05ee9afad204565fa54284ceb5515cb0e8ace
Aditya Yadav <adiyadav.902@gmail.com>
2022-10-29 00:29:57 +0530

    React-Query-Initialised

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const Home = () => {
+  const { data } = useQuery(["data"], () =>
+    axios.get("https://reqres.in/api/users?page=1")
+  );
+
+  console.log("data", data && data);
+
   return <div>Kalazara Education</div>;
 };
 
