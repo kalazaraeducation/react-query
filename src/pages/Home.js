c1ab42df19d98a1a46d21c021edc667d10330d9c
Aditya Yadav <adiyadav.902@gmail.com>
2022-10-29 19:14:26 +0530

    loading_and_error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const Home = () => {
-  const { data } = useQuery(["data"], () =>
-    axios.get("https://reqres.in/api/users?page=1")
+  const { data, isLoading, isError, error } = useQuery(["data"], () =>
+    axios.get("https://reqres.in/api1/users?page=1")
   );
 
   console.log("data", data && data);
 
-  return <div>Kalazara Education</div>;
+  if (isError) return <h1>{error.message}</h1>;
+
+  if (isLoading) return <h1>Loading...</h1>;
+
+  return (
+    <>
+      {data?.data.data.map((item) => (
+        <p>{item.email}</p>
+      ))}
+    </>
+  );
 };
 
 export default Home;
